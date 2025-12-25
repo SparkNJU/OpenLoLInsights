2e336b9d2ac25a9a5c13f0c4b655640f85e2e077
Zenkinin <231830135@smail.nju.edu.cn>
2025-12-25 16:32:40 +0800

    解决白屏问题

--- LoLSmartStatsWeb/frontend/src/views/layout/Layout.vue
+++ LoLSmartStatsWeb/frontend/src/views/layout/Layout.vue
@@ -135,9 +135,7 @@ const handleLogout = () => {
       <!-- Router View 容器 -->
       <div class="flex-1 overflow-hidden relative">
         <router-view v-slot="{ Component }">
-          <transition name="fade" mode="out-in">
-            <component :is="Component" />
-          </transition>
+            <component :is="Component" :key="route.fullPath"/>
         </router-view>
       </div>
     </main>
@@ -146,13 +144,5 @@ const handleLogout = () => {
 </template>
 
 <style scoped>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.2s ease;
-}
 
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
 </style>
